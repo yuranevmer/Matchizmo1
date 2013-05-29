4f89f70ea00771b9244811d43bcdb382fe40bc40
yuranevmer <yura.bag@gmail.com>
2013-05-29 15:54:51 +0300

    synk

---
+++ Matchizmo/SecondCardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondCardGameViewController.h
+//  Matchizmo
+//
+//  Created by yura on 5/29/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondCardGameViewController : UIViewController
+
+@end
---
+++ Matchizmo/SecondCardGameViewController.m
@@ -0,0 +1,38 @@
+//
+//  SecondCardGameViewController.m
+//  Matchizmo
+//
+//  Created by yura on 5/29/13.
+//
+//
+
+#import "SecondCardGameViewController.h"
+
+@interface SecondCardGameViewController ()
+
+@end
+
+@implementation SecondCardGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
